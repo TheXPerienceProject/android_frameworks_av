697a52120bd459dcb61b57b7414b96424bccfc48
klozz <shaclone707@gmail.com>
2018-09-01 04:58:23 -0400

    Revert "libstagefright: Initialize port modes for extradata ports"
    This reverts commit 33b9b43fdbda38e98f713dba5099ad53f51b83d8.

--- media/libstagefright/omx/include/media/stagefright/omx/OMXNodeInstance.h
+++ media/libstagefright/omx/include/media/stagefright/omx/OMXNodeInstance.h
@@ -152,9 +152,9 @@ private:
     KeyedVector<OMX_BUFFERHEADERTYPE *, IOMX::buffer_id> mBufferHeaderToBufferID;
 
     bool mLegacyAdaptiveExperiment;
-    IOMX::PortMode mPortMode[4];
+    IOMX::PortMode mPortMode[2];
     // metadata and secure buffer types and graphic buffer mode tracking
-    MetadataBufferType mMetadataType[4];
+    MetadataBufferType mMetadataType[2];
     enum SecureBufferType {
         kSecureBufferTypeUnknown,
         kSecureBufferTypeOpaque,
