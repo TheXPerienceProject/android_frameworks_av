444d9254cd89832f364e8c12718ad146ee40f1e9
Murtuza Raja <mraja@qti.qualcomm.com>
2025-09-29 05:15:22 -0700

    Revert "Camera: Wait for the HEIC gainmap format callback"
    This reverts commit 7f4c165cb9916472dde68060b058dac62f53c672.
    
    Change-Id: Ibb143f9a9a3c51d0c1054b43ad77602f5884f509

--- services/camera/libcameraservice/api2/HeicCompositeStream.cpp
+++ services/camera/libcameraservice/api2/HeicCompositeStream.cpp
@@ -1109,7 +1109,6 @@ status_t HeicCompositeStream::processInputFrame(int64_t frameNumber,
     bool hasOutputBuffer = inputFrame.muxer != nullptr ||
             (mDequeuedOutputBufferCnt < kMaxOutputSurfaceProducerCount);
     bool hasGainmapMetadata = !inputFrame.isoGainmapMetadata.empty();
-    bool hdrGainmapFormatReady = mHDRGainmapEnabled ? (mGainmapFormat != nullptr) : true;
 
     ALOGV("%s: [%" PRId64 "]: appSegmentReady %d, codecOutputReady %d, codecInputReady %d,"
             " dequeuedOutputBuffer %d, timestamp %" PRId64, __FUNCTION__, frameNumber,
@@ -1142,13 +1141,6 @@ status_t HeicCompositeStream::processInputFrame(int64_t frameNumber,
                     strerror(-res), res);
             return res;
         }
-    }
-
-    if (!hdrGainmapFormatReady) {
-        // If HDR gainmap is enabled, we need to wait until the gainmap format
-        // is received from the codec before starting the muxer. Otherwise,
-        // the muxer will be able to add the gainmap track.
-        return OK;
     }
 
     if (!(codecOutputReady && hasOutputBuffer) && !appSegmentReady) {
